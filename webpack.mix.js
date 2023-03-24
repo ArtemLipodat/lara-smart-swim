708fbd91cd2f6f19447f7d7567929dd5fc6f1562
Artem Lipodat <a.lipodat@mail.ru>
2023-03-24 21:30:20 +0400

    setts

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,18 @@ let mix = require('laravel-mix');
 
 mix.browserSync('smart.local');
 
+
+
 mix.js('resources/js/app.js', 'js').sass('resources/sass/app.scss', 'css');
+
+
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.(svg)$/,
+                use: "svg-sprite-loader"
+            },
+        ]
+    },
+});
